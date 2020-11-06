c186e0d8b313e792363eadd84dc4eb7da7e4a42e
mi11km <mi11km.basket@gmail.com>
2020-11-06 13:00:29 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,51 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timeID: NodeJS.Timeout;
+    timeID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timeID);
+    };
+  }, []); // [] を第２引数に渡すことで マウント時とアンマウント時のみにできる。
+
+  return <h1 className="clock">{date.toLocaleString()}</h1>;
+}
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>はじめてのNext.js with Typescript</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+  const [url, setUrl] = useState("https:example.com");
 
-            <main className={styles.main}>
-                はろー
-            </main>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  };
+  const handleSubmit = () => {
+    open(url);
+  };
 
-            <footer className={styles.footer}>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>はじめてのNext.js with Typescript</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            </footer>
-        </div>
-    )
+      <main className={styles.main}>
+        <Clock />
+
+        <form onSubmit={handleSubmit}>
+          <h2>urlを入力してください</h2>
+          <input type="text" value={url} onChange={handleChange} />
+          <input type="submit" value="開く" />
+        </form>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
 }
