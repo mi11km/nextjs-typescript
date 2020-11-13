3b1d719abf36436ecfe0bfca5529a0b104f0150c
mi11km <mi11km.basket@gmail.com>
2020-11-13 17:36:34 +0900

    create _document.tsx and src dir

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head></Head>
+        <body>
+          <Main></Main>
+          <NextScript></NextScript>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
