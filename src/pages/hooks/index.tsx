837569ad6edc9db219a672aaeac57dfef211519f
mi11km <mi11km.basket@gmail.com>
2020-12-04 17:34:14 +0900

    useReducer

--- src/pages/hooks/index.tsx
+++ src/pages/hooks/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
+
+import reducer, { EventType } from '../../reducers';
 
 const Hooks: React.FC = () => {
+    const [state, dispatch] = useReducer(reducer, []);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const addEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        dispatch({
+            type: EventType.CREATE_EVENT,
+            title: title,
+            body: body
+        });
+        setTitle('');
+        setBody('');
+    };
+
     return (
         <React.Fragment>
             <div>
                 <h2>イベント作成フォーム</h2>
                 <form>
                     <label htmlFor="formEventTitle">タイトル</label>
-                    <input type="text" id="formEventTitle" />
+                    <input
+                        type="text"
+                        id="formEventTitle"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                     <br />
                     <label htmlFor="formEventBody">ボディー</label>
-                    <textarea name="body" id="formEventBody" cols={30} rows={5} />
+                    <textarea
+                        name="body"
+                        id="formEventBody"
+                        cols={30}
+                        rows={5}
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    />
 
                     <br />
-                    <button>イベントを作成する</button>
+                    <button onClick={addEvent}>イベントを作成する</button>
                     <button>全てのイベントを削除する</button>
                 </form>
 
